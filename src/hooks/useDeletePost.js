3db7e2b4ed343390886089fbd793923fa4629cc9
deepak bhatt <deepakbhattmits@gmail.com>
2021-05-11 08:46:52 +0530

    Update useDeletePost.js

--- src/hooks/useDeletePost.js
+++ src/hooks/useDeletePost.js
@@ -1,20 +1,40 @@
-import { useReducer, useCallback } from 'react'
-import axios from 'axios'
+// import { useReducer, useCallback } from 'react';
+import { queryCache, useMutation } from 'react-query';
+import axios from 'axios';
 
 export default function useDeletePost() {
-  const [state, setState] = useReducer((_, action) => action, {
-    isIdle: true,
-  })
+  // const [state, setState] = useReducer((_, action) => action, {
+  //   isIdle: true,
+  // })
 
-  const mutate = useCallback(async (postId) => {
-    setState({ isLoading: true })
-    try {
-      await axios.delete(`/api/posts/${postId}`).then((res) => res.data)
-      setState({ isSuccess: true })
-    } catch (error) {
-      setState({ isError: true, error })
+  // const mutate = useCallback(async (postId) => {
+  //   setState({ isLoading: true })
+  //   try {
+  //     await axios.delete(`/api/posts/${postId}`).then((res) => res.data)
+  //     setState({ isSuccess: true })
+  //   } catch (error) {
+  //     setState({ isError: true, error })
+  //   }
+  // }, [])
+  // return [mutate, state]
+  const [deletePost, deletePostInfo] = useMutation(
+    (postId) => axios.delete(`/api/posts/${postId}`).then((res) => res.data),
+    {
+      onMutate: (postId) => {
+        // queryCache.setQueryData(['post', postId], values)
+        // queryCache.invalidateQueries(['post', postId]);
+      },
+      onSuccess: (data, postId) => {
+        // console.log('DATA : ', data)
+        queryCache.setQueryData(['post', postId], data);
+        queryCache.invalidateQueries(['post', postId]);
+      },
+      onError: (error, postId) => {
+        // console.log('ERROR : ', error.response.data)
+        // alert(error.response.data.message)
+      },
+      onSettled: (data, error, postId) => {},
     }
-  }, [])
-
-  return [mutate, state]
+  );
+  return [deletePost, deletePostInfo];
 }
