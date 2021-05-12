d6a7e4f76a9fd99129d840ac1427cd610a1fcdcb
deepak bhatt <deepakbhattmits@gmail.com>
2021-05-12 08:00:09 +0530

    Update [postId].js
    refactor

--- pages/api/posts/[postId].js
+++ pages/api/posts/[postId].js
@@ -1,102 +1,102 @@
-import db from '../../../db'
-import { sleep } from '../../../utils'
+import db from '../../../db';
+import { sleep } from '../../../utils';
 
-const deleteFailureRate = 0
+const deleteFailureRate = 0;
 
 export default async (req, res) => {
-  await sleep(1000)
+  await sleep(1000);
 
   try {
     if (req.method === 'GET') {
-      return await GET(req, res)
+      return await GET(req, res);
     } else if (req.method === 'PATCH') {
-      return await PATCH(req, res)
+      return await PATCH(req, res);
     } else if (req.method === 'DELETE') {
-      return await DELETE(req, res)
+      return await DELETE(req, res);
     }
   } catch (err) {
-    console.error(err)
-    res.status(500)
-    res.json({ message: 'An unknown error occurred!' })
+    console.error(err);
+    res.status(500);
+    res.json({ message: 'An unknown error occurred!' });
   }
-}
+};
 
-async function GET(req, res) {
+const GET = async (req, res) => {
   const {
     query: { postId },
-  } = req
+  } = req;
 
-  const row = (await db.get()).posts.find((d) => d.id == postId)
+  const row = (await db.get()).posts.find((d) => d.id == postId);
 
   if (!row) {
-    res.status(404)
-    return res.send('Not found')
+    res.status(404);
+    return res.send('Not found');
   }
 
-  res.json(row)
-}
+  res.json(row);
+};
 
-async function PATCH(req, res) {
+const PATCH = async (req, res) => {
   const {
     query: { postId },
     body,
-  } = req
+  } = req;
 
   if (body.body.includes('fail')) {
-    res.status(500)
-    res.json({ message: 'An unknown error occurred!' })
-    return
+    res.status(500);
+    res.json({ message: 'An unknown error occurred!' });
+    return;
   }
 
-  const row = (await db.get()).posts.find((d) => d.id == postId)
+  const row = (await db.get()).posts.find((d) => d.id == postId);
 
   if (!row) {
-    res.status(404)
-    return res.send('Not found')
+    res.status(404);
+    return res.send('Not found');
   }
 
-  delete body.id
+  delete body.id;
 
   const newRow = {
     ...row,
     ...body,
-  }
+  };
 
   await db.set((old) => {
     return {
       ...old,
       posts: old.posts.map((d) => (d.id == postId ? newRow : d)),
-    }
-  })
+    };
+  });
 
-  res.json(newRow)
-}
+  res.json(newRow);
+};
 
-async function DELETE(req, res) {
+const DELETE = async (req, res) => {
   const {
     query: { postId },
-  } = req
+  } = req;
 
   if (Math.random() < deleteFailureRate) {
-    res.status(500)
-    res.json({ message: 'An unknown error occurred!' })
-    return
+    res.status(500);
+    res.json({ message: 'An unknown error occurred!' });
+    return;
   }
 
-  const row = (await db.get()).posts.find((d) => d.id == postId)
+  const row = (await db.get()).posts.find((d) => d.id == postId);
 
   if (!row) {
-    res.status(404)
-    return res.send('Not found')
+    res.status(404);
+    return res.send('Not found');
   }
 
   await db.set((old) => {
     return {
       ...old,
       posts: old.posts.filter((d) => d.id != postId),
-    }
-  })
+    };
+  });
 
-  res.status(200)
-  res.send('Resource Deleted')
-}
+  res.status(200);
+  res.send('Resource Deleted');
+};
