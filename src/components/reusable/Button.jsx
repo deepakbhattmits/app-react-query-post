c00d55f1b9149a7035df89f7237ac3a5d86b9f3e
deepak bhatt <deepakbhattmits@gmail.com>
2021-05-12 08:00:48 +0530

    Update Button.jsx

--- src/components/reusable/Button.jsx
+++ src/components/reusable/Button.jsx
@@ -1,6 +1,6 @@
-const Button = ({ children, type, className, onClick = () => {} }) => (
+const Button = ({ children, type = '', className, onClick = () => {} }) => (
   <button type={type} className={`ui button ${className}`} onClick={onClick}>
     {children}
   </button>
-)
-export default Button
+);
+export default Button;
