5943a014df3ecbf0e34e0761bfbe2bc09ce1c0b4
deepak bhatt <deepakbhattmits@gmail.com>
2021-05-12 08:00:31 +0530

    Update GlobalLoader.jsx

--- src/components/GlobalLoader.jsx
+++ src/components/GlobalLoader.jsx
@@ -1,19 +1,18 @@
-import { Loader } from './styled'
+import { Loader } from './styled';
 
-const GlobalLoader = () => {
-  return (
-    <Loader
-      css={`
-        position: absolute;
-        top: 0.5rem;
-        right: 0.5rem;
-        font-size: 1.5rem;
-        transition: 0.3s ease;
-      `}
-      style={{
-        opacity: 1,
-      }}
-    />
-  )
-}
-export default GlobalLoader
+const GlobalLoader = () => (
+  <Loader
+    css={`
+      position: absolute;
+      top: 0.5rem;
+      right: 0.5rem;
+      font-size: 1.5rem;
+      transition: 0.3s ease;
+    `}
+    style={{
+      opacity: 1,
+    }}
+  />
+);
+
+export default GlobalLoader;
