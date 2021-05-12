2817128e868cc9d3627430f369c74bcf08ac0871
deepak bhatt <deepakbhattmits@gmail.com>
2021-05-12 07:59:57 +0530

    Update index.js
    refactor

--- db/index.js
+++ db/index.js
@@ -1,19 +1,17 @@
-import fs from 'fs-extra'
-import path from 'path'
+import fs from 'fs-extra';
+import path from 'path';
 
-const storeLocation = path.resolve(process.cwd(), 'store.json')
+const storeLocation = path.resolve(process.cwd(), 'store.json');
+
+const set = async (updater) => {
+  const file = await fs.readJSON(storeLocation);
+  const newFile = updater(file);
+  await fs.writeJSON(storeLocation, newFile);
+};
+
+const get = async () => await fs.readJSON(storeLocation);
 
 export default {
   set,
   get,
-}
-
-async function set(updater) {
-  const file = await fs.readJSON(storeLocation)
-  const newFile = updater(file)
-  await fs.writeJSON(storeLocation, newFile)
-}
-
-function get() {
-  return fs.readJSON(storeLocation)
-}
+};
