b3d534d2146507a1401eb63845a49422c48461ef
deepak bhatt <deepakbhattmits@gmail.com>
2021-05-12 08:01:11 +0530

    Update useDeletePost.js

--- src/hooks/useDeletePost.js
+++ src/hooks/useDeletePost.js
@@ -1,22 +1,7 @@
-// import { useReducer, useCallback } from 'react';
 import { queryCache, useMutation } from 'react-query';
 import axios from 'axios';
 
 export default function useDeletePost() {
-  // const [state, setState] = useReducer((_, action) => action, {
-  //   isIdle: true,
-  // })
-
-  // const mutate = useCallback(async (postId) => {
-  //   setState({ isLoading: true })
-  //   try {
-  //     await axios.delete(`/api/posts/${postId}`).then((res) => res.data)
-  //     setState({ isSuccess: true })
-  //   } catch (error) {
-  //     setState({ isError: true, error })
-  //   }
-  // }, [])
-  // return [mutate, state]
   const [deletePost, deletePostInfo] = useMutation(
     (postId) => axios.delete(`/api/posts/${postId}`).then((res) => res.data),
     {
