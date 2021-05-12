1ccac7f6cf8647771c94c8e208f0f427b923ddee
deepak bhatt <deepakbhattmits@gmail.com>
2021-05-12 08:01:51 +0530

    Update useSavePost.js
    added useMutation hooks logic

--- src/hooks/useSavePost.js
+++ src/hooks/useSavePost.js
@@ -1,36 +1,18 @@
-// import { useReducer, useCallback } from 'react'
-import { queryCache, useMutation } from 'react-query'
-import axios from 'axios'
+import { queryCache, useMutation } from 'react-query';
+import axios from 'axios';
 
 export default function useSavePost() {
-  // const [state, setState] = useReducer((_, action) => action, {
-  //   isIdle: true,
-  // })
-
-  // const mutate = useCallback(async (values) => {
-  //   setState({ isLoading: true })
-  //   try {
-  //     const data = await axios
-  //       .patch(`/api/posts/${values.id}`, values)
-  //       .then((res) => res.data)
-  //     setState({ isSuccess: true, data })
-  //   } catch (error) {
-  //     setState({ isError: true, error })
-  //   }
-  // }, [])
-
-  // return [mutate, state]
   const [savePost, savePostInfo] = useMutation(
     (values) =>
       axios.patch(`/api/posts/${values.id}`, values).then((res) => res.data),
     {
       onMutate: (values) => {
-        queryCache.setQueryData(['post', values.id], values)
+        queryCache.setQueryData(['post', values.id], values);
       },
       onSuccess: (data, values) => {
         // console.log('DATA : ', data)
-        queryCache.setQueryData(['post', values.id], data)
-        queryCache.invalidateQueries(['post', values.id])
+        queryCache.setQueryData(['post', values.id], data);
+        queryCache.invalidateQueries(['post', values.id]);
       },
       onError: (error, values) => {
         // console.log('ERROR : ', error.response.data)
@@ -38,6 +20,6 @@ export default function useSavePost() {
       },
       onSettled: (data, error, values) => {},
     }
-  )
-  return [savePost, savePostInfo]
+  );
+  return [savePost, savePostInfo];
 }
