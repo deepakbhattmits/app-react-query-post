0821f2a0dad634debb034f4a212a6423d048a7b3
deepak bhatt <deepakbhattmits@gmail.com>
2021-05-12 08:01:21 +0530

    Update usePosts.js

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,33 +1,27 @@
-import { useReducer, useEffect } from 'react'
-import axios from 'axios'
+import { useReducer, useEffect } from 'react';
+import axios from 'axios';
 
 export default function usePosts() {
   const [state, setState] = useReducer((_, action) => action, {
     isLoading: true,
-  })
+  });
 
   const fetch = async () => {
-    setState({ isLoading: true })
+    setState({ isLoading: true });
     try {
-      const data = await axios.get('/api/posts').then((res) => res.data)
-      setState({ isSuccess: true, data })
+      const data = await axios.get('/api/posts').then((res) => res.data);
+      setState({ isSuccess: true, data });
     } catch (error) {
-      setState({ isError: true, error })
+      setState({ isError: true, error });
     }
-  }
+  };
 
   useEffect(() => {
-    let unmounted = false
-    if (!unmounted) {
-      fetch()
-    }
-    return () => {
-      unmounted = true
-    }
-  }, [])
+    fetch();
+  }, []);
 
   return {
     ...state,
     fetch,
-  }
+  };
 }
