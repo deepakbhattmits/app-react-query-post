4f43f07f2220a7908f9cc35420264accf0694657
deepakbhattmits <deepakbhattmits@gmail.com>
2022-02-07 10:31:09 +0530

    Create usePost.js

---
+++ src__/hooks/usePost.js
@@ -0,0 +1,36 @@
+import { useReducer, useCallback, useEffect } from 'react';
+import axios from 'axios';
+
+export const fetchPost = (postId) =>
+  axios.get(`/api/posts/${postId}`).then((res) => res.data);
+
+export default function usePost(postId) {
+  const [state, setState] = useReducer((_, action) => action, {
+    isLoading: true,
+  });
+
+  const fetch = useCallback(async () => {
+    setState({ isLoading: true });
+    try {
+      const data = await fetchPost(postId);
+      setState({ isSuccess: true, data });
+    } catch (error) {
+      setState({ isError: true, error });
+    }
+  }, [postId]);
+
+  useEffect(() => {
+    let unmounted = false;
+    if (!unmounted) {
+      fetch();
+    }
+    return () => {
+      unmounted = true;
+    };
+  }, []);
+
+  return {
+    ...state,
+    fetch,
+  };
+}
