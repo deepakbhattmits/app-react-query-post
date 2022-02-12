c73b75794e0f6fdc8a78d0cd787bce622d16ae72
deepakbhattmits <deepakbhattmits@gmail.com>
2022-02-12 12:59:10 +0530

    Create useCreatePost_bkup.js

---
+++ src__/hooks/useCreatePost_bkup.js
@@ -0,0 +1,19 @@
+import { useReducer, useCallback } from 'react';
+import axios from 'axios';
+
+export default function useCreatePost() {
+  const [state, setState] = useReducer((_, action) => action, {
+    isIdle: true,
+  });
+
+  const mutate = useCallback(async (values) => {
+    setState({ isLoading: true });
+    try {
+      const data = axios.post('/api/posts', values).then((res) => res.data);
+      setState({ isSuccess: true, data });
+    } catch (error) {
+      setState({ isError: true, error });
+    }
+  }, []);
+  return [mutate, state];
+}
