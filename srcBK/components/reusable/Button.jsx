846fa4de360e05f91f23d8d610f2f86b9d09e8ff
deepakbhattmits <deepakbhattmits@gmail.com>
2022-02-12 13:00:07 +0530

    Delete Button.jsx

--- srcBK/components/reusable/Button.jsx
+++
@@ -1,6 +0,0 @@
-const Button = ({ children, type = '', className, onClick = () => {} }) => (
-  <button type={type} className={`ui button ${className}`} onClick={onClick}>
-    {children}
-  </button>
-);
-export default Button;
