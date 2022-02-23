350c24b38a7d738dfbc81a3a492bd3970fa0ec5c
deepakbhattmits <deepakbhattmits@gmail.com>
2022-02-23 08:37:14 +0530

    Create index.jsx

---
+++ src__/screens/blog/index.jsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+// Hooks
+import usePosts from '../../hooks/usePosts';
+import { PostStyles } from '../../components/styled';
+
+const Home = () => {
+  const postsQuery = usePosts();
+
+  return (
+    <>
+      <h1>Blog</h1>
+
+      <div
+        css={`
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 1rem;
+        `}
+      >
+        {postsQuery.isLoading ? (
+          <span>Loading...</span>
+        ) : postsQuery.isError ? (
+          postsQuery.error.message
+        ) : (
+          postsQuery.data.map((post) => (
+            <PostStyles as={Link} to={`./${post.id}`} key={post.id}>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </PostStyles>
+          ))
+        )}
+      </div>
+    </>
+  );
+};
+export default Home;
