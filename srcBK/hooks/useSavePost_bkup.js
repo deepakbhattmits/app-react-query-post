83cb498265a52f58c965adb70c1251de5f59ba05
deepakbhattmits <deepakbhattmits@gmail.com>
2022-02-26 10:52:11 +0530

    Create useSavePost_bkup.js

---
+++ srcBK/hooks/useSavePost_bkup.js
@@ -0,0 +1,22 @@
+import { useReducer, useCallback } from 'react';
+import axios from 'axios';
+
+export default function useSavePost() {
+  const [state, setState] = useReducer((_, action) => action, {
+    isIdle: true,
+  });
+
+  const mutate = useCallback(async (values) => {
+    setState({ isLoading: true });
+    try {
+      const data = await axios
+        .patch(`/api/posts/${values.id}`, values)
+        .then((res) => res.data);
+      setState({ isSuccess: true, data });
+    } catch (error) {
+      setState({ isError: true, error });
+    }
+  }, []);
+
+  return [mutate, state];
+}
